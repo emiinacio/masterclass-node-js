f3d7314955a514d03e19097b2f15609b9f530595
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-26 09:44:10 +0000

    first commit

---
+++ stats.js
@@ -0,0 +1,19 @@
+const os = require('os');
+
+const { freemem, totalmem } = os
+
+const total = parseInt(totalmem() / 1024 /1024)
+const mem = parseInt(freemem() / 1024 / 1024)
+const percents = parseInt((mem / total) * 100)
+
+console.log(mem, total, percents)
+
+
+const stats = {
+    free: `${mem} MB`,
+    total: `{total} MB`,
+    usage: `${percents}%`
+}
+
+console.table('=== PC STATS ===')
+console.table(stats)
