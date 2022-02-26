85ddc26d8014e7af658ce376e94eda306411122e
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-26 10:14:01 +0000

    end logger

--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require('os');
+const log = require('./logger');
 
 setInterval(() => {
     const { freemem, totalmem } = os
@@ -19,6 +20,7 @@ setInterval(() => {
     console.clear()
     console.table('=== PC STATS ===')
     console.table(stats)
-    
+
+    log(`${JSON.stringify(stats)}\n`)
 }, 1000)
 
