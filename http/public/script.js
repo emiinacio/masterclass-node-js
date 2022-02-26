c82ba55688a192de924b219f45b2ccd6a16d3f24
emily <emily@xhockware.lan>
2022-02-26 18:06:43 +0000

    finish

--- http/public/script.js
+++ http/public/script.js
@@ -5,14 +5,23 @@ const form = document.querySelector('form')
 
 async function load() {
     const res = await fetch("http://localhost:3000/").then((data) => data.json())
-    
-    res.urls.map(({name, url}) => addElement({name, url}))
+
+    res.urls.map(({
+        name,
+        url
+    }) => addElement({
+        name,
+        url
+    }))
 }
 
 load()
 
 
-function addElement({ name, url }) {
+function addElement({
+    name,
+    url
+}) {
     const li = document.createElement('li')
     const a = document.createElement("a")
     const trash = document.createElement("span")
@@ -37,20 +46,25 @@ function removeElement(el) {
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let { value } = input
+    let {
+        value
+    } = input
 
-    if (!value) 
+    if (!value)
         return alert('Preencha o campo')
 
     const [name, url] = value.split(",")
 
-    if (!url) 
+    if (!url)
         return alert('formate o texto da maneira correta')
 
-    if (!/^http/.test(url)) 
+    if (!/^http/.test(url))
         return alert("Digite a url da maneira correta")
 
-    addElement({ name, url })
+    addElement({
+        name,
+        url
+    })
 
     input.value = ""
 })
