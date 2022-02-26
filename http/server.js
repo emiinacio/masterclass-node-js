6834ba433ea63a4b6fb32712f0a1ee538ac259ae
emiinacio <emilymarcelino59@gmail.com>
2022-02-26 15:39:07 +0000

    add http

--- http/server.js
+++ http/server.js
@@ -3,9 +3,22 @@ const fs = require('fs');
 const path = require('path');
 
 http.createServer((req, res) => {
-    if(req.url === '/') {
-        fs.readFile(
-            path.join(__dirname, 'public', 'index.html')
-        )
-    }
+
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+    const extname = path.extname(filePath)
+
+    const allowedFileTypes = ['.html', '.css', '.js']
+    const allowed = allowedFileTypes.find(item => item == extname)
+
+    if(!allowed) return
+    
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if (err) throw err
+
+            res.end(content)
+        }
+    )
 }).listen(5000, () => console.log('Server is running'))
