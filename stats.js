59e44f7498c66b3e7462c411b4e53ac3a9b0e7a9
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-26 09:51:10 +0000

    end stats

--- stats.js
+++ stats.js
@@ -1,19 +1,24 @@
 const os = require('os');
 
-const { freemem, totalmem } = os
+setInterval(() => {
+    const { freemem, totalmem } = os
 
-const total = parseInt(totalmem() / 1024 /1024)
-const mem = parseInt(freemem() / 1024 / 1024)
-const percents = parseInt((mem / total) * 100)
+    const total = parseInt(totalmem() / 1024 /1024)
+    const mem = parseInt(freemem() / 1024 / 1024)
+    const percents = parseInt((mem / total) * 100)
 
-console.log(mem, total, percents)
+    console.log(mem, total, percents)
 
 
-const stats = {
-    free: `${mem} MB`,
-    total: `{total} MB`,
-    usage: `${percents}%`
-}
+    const stats = {
+        free: `${mem} MB`,
+        total: `{total} MB`,
+        usage: `${percents}%`
+    }
 
-console.table('=== PC STATS ===')
-console.table(stats)
+    console.clear()
+    console.table('=== PC STATS ===')
+    console.table(stats)
+    
+}, 1000)
+
